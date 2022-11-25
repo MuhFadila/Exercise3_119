7589d39fb061520920a8233259ad4405e0595a37
Muhammad Fadila <muh.fadila18@gmail.com>
2022-11-25 16:03:23 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise3_119
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
